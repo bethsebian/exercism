c638e43fbbf05768d038a4212357e0d40b736d69
Beth Sebian <esebian@gmail.com>
2016-05-10 18:54:23 -0600

    Clarify ArgumentError message and refactor of_dna

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -2,23 +2,22 @@ class Complement
   VERSION = 3
 
   def self.of_dna(nucleotides)
-    nucleotides.chars.map { |nucleotide| find_complement(nucleotide) }.join
-  end
-
-  def self.find_complement(nucleotide)
-    verify(nucleotide)
-    complements[nucleotide]
+    nucleotides.chars.map do |nucleotide|
+      verify(nucleotide)
+      complements[nucleotide]
+    end.join
   end
 
   def self.verify(nucleotide)
-    raise ArgumentError if !complements.key?(nucleotide)
+    warning = "Nucleotide '#{nucleotide}' not recognized"
+    raise ArgumentError, warning if !complements.key?(nucleotide)
   end
 
   def self.complements
-    pairs = { "G" => "C",
-              "C" => "G",
-              "T" => "A",
-              "A" => "U"
-              }
+    { "G" => "C",
+      "C" => "G",
+      "T" => "A",
+      "A" => "U"
+    }
   end
 end
