70ebd20b96909fa67ca98eda56f9881d67b90189
Beth Sebian <esebian@gmail.com>
2016-05-10 18:35:17 -0600

    Remove pry and refactor

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,16 +1,12 @@
-require 'pry'
-
 class Hamming
   VERSION = 1
 
-  def self.compute(s_1, s_2)
-    raise ArgumentError, "Strand length mismatch" if s_1.length != s_2.length
-    discrepancies_count(s_1, s_2)
+  def self.compute(str_1, str_2)
+    raise ArgumentError, "Strand length mismatch" if str_1.length != str_2.length
+    discrepancies_count(str_1, str_2)
   end
 
-  def self.discrepancies_count(s_1, s_2)
-    (0..s_1.length).reject do |i|
-      s_1[i] == s_2[i]
-    end.count
+  def self.discrepancies_count(str_1, str_2)
+    (0..str_1.length).reject { |i| str_1[i] == str_2[i] }.count
   end
 end
