cfdb05486bc0891032a6ee29e1c27013f0157c2f
Beth Sebian <esebian@gmail.com>
2016-05-13 22:10:14 -0600

    Refactor ruby/pangram

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,9 +1,9 @@
 class Pangram
   VERSION = 1
 
-  def self.is_pangram?(str)
+  def self.is_pangram?(sentence)
     alphabet_letters = ("a".."z").to_a
-    str.downcase.chars.each { |letter| alphabet_letters.delete(letter) }
+    sentence.downcase.each_char { |letter| alphabet_letters.delete(letter) }
     alphabet_letters.empty?
   end
 end
