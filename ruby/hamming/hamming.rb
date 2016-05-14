207367b9b00d2f415cd415cd201abefa08833e6a
Beth Sebian <esebian@gmail.com>
2016-05-13 21:22:02 -0600

    Implement ruby/hamming with indexing

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -3,8 +3,7 @@ module Hamming
 
   def self.compute(strand_1, strand_2)
     require_same_lengths(strand_1, strand_2)
-    position_pairs = strand_1.each_char.zip(strand_2.each_char)
-    position_pairs.count { |strand_1, strand_2| strand_1 != strand_2 }
+    (0...strand_1.length).count { |i| strand_1[i] != strand_2[i] }
   end
 
   class << self
