7043c9a2d0c20bd3e5690f9350d630e1dbaef48b
Beth Sebian <esebian@gmail.com>
2016-05-16 19:09:13 -0600

    Refactor ruby/hamming

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,16 +2,8 @@ module Hamming
   VERSION = 1
 
   def self.compute(strand_1, strand_2)
-    require_same_lengths(strand_1, strand_2)
+    alert = "Strand length mismatch (#{strand_1.length} v #{strand_2.length})"
+    fail ArgumentError, alert unless strand_1.length == strand_2.length
     (0...strand_1.length).count { |i| strand_1[i] != strand_2[i] }
-  end
-
-  class << self
-
-  private
-    def require_same_lengths(strand_1, strand_2)
-      alert = "Strand length error: '#{strand_1}' length != '#{strand_2}' length"
-      fail ArgumentError, alert unless strand_1.length == strand_2.length
-    end
   end
 end
