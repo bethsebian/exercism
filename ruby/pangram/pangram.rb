75389c73f64d485efa4fc6a3e60281c1cac46d17
Beth Sebian <esebian@gmail.com>
2016-05-16 18:14:37 -0600

    Update ruby/pangram

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -2,7 +2,6 @@ module Pangram
   VERSION = 1
 
   def self.is_pangram?(sentence)
-    letters = ("a".."z").to_a
-    letters.reject { |letter| sentence.downcase.include?(letter) }.empty?
+    ("a".."z").to_a.reject {|letter| sentence.downcase.include?(letter)}.empty?
   end
 end
