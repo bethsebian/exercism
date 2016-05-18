5fa7812d0b2dbf1d68575c4ae14869da4e592a74
Beth Sebian <esebian@gmail.com>
2016-05-17 19:46:26 -0600

    Refactor ruby/roman_numerals

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,30 +1,34 @@
 module RomanNumerals
   VERSION = 1
-  INFO = {"ones"     => { "nearest_5" => { 0 => "", 5 => "V", 10 => "X" },
-                          "adjuster"  => "I" },
-          "tens"     => { "nearest_5" => { 0 => "", 5 => "L", 10 => "C" },
-                          "adjuster"  => "X" },
-          "hundreds" => { "nearest_5" => { 0 => "", 5 => "D", 10 => "M" },
-                          "adjuster"  => "C" },
-          "thousands"=> { "nearest_5" => { 0 => "" },
-                          "adjuster"  => "M" }}
-  PLACE_POSITIONS = { "thousands" => -4, "hundreds" => -3, "tens" => -2, "ones" => -1 }
+  ANCHOR = {1 => { "anchor" => { 0 => "", 5 => "V", 10 => "X" }, 
+                   "adjuster" => 1 },
+            2 => { "anchor" => { 0 => "", 5 => "L", 10 => "C" },
+                   "adjuster" => 1 },
+            3 => { "anchor" => { 0 => "", 5 => "D", 10 => "M" },
+                   "adjuster" => 1 },
+            4 => { "anchor" => { 0 => "" },
+                   "adjuster" => 1 }
+  ADJUSTER = { 1 => "I", 2 => "X", 3 => "C", 4 => "M" }
 
   def to_roman
-    # place_values = self.to_s.chars.map(&:to_i)
-
-    place_values = PLACE_POSITIONS.map do |place, position|    #split into array  (need number and position, or full number)
-      [place, self.to_s.split('')[position].to_i]
-    end
-    place_values.map { |place, value| romans(place, value) }.join # grab numbers
+    place_values = self.to_s.chars.map(&:to_i)
+    place_values.map.with_index do |value, i|
+      place = self.to_s.length - i
+      romans(place, value)
+    end.join
   end
 
   def romans(place, value)
     nearest_5 = roman_round(value)
     diff = value - nearest_5
-    anchor = INFO[place]["nearest_5"][nearest_5]
-    adjustment = INFO[place]["adjuster"] * diff.abs
-    diff < 0 ? (adjustment + anchor) : (anchor + adjustment)
+    anchor = ANCHOR[place][nearest_5]       #valid_placeholder
+    adjustment = ADJUSTER[place] * diff.abs #roman_fill #reduce
+    order_numerals(adjustment, anchor, diff)
+  end
+
+  def order_numerals(adjustment, anchor, diff)
+    return adjustment + anchor if diff < 0
+    return anchor + adjustment if diff >= 0
   end
 
   def roman_round(num)
