1744b9a4a0ce1a1c6363977707d85aeeb2764db4
Beth Sebian <esebian@gmail.com>
2016-05-22 00:16:49 -0600

    Refactor ruby/robot-name

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -4,23 +4,28 @@ class Robot
   @@all_names = []
 
   def initialize
-    @name = verified_name
-    @@all_names << @name
-  end
-
-  def verified_name
-    candidate = set_name
-    @@all_names.any? { |name| name == candidate } ? verified_name : candidate
-  end
-
-  def set_name
-    letters = 2.times.map { ("A".."Z").to_a.sample }.join
-    numbers = 3.times.map { (1..9).to_a.sample.to_s }.join
-    letters + numbers
+    set_name
   end
 
   def reset
-    @name = verified_name
+    set_name
   end
+
+  private
+    def set_name
+      @name = verified_name
+      @@all_names << @name
+    end
+
+    def verified_name
+      candidate = name_candidate
+      @@all_names.any? { |name| name == candidate } ? verified_name : candidate
+    end
+
+    def name_candidate
+      letters = 2.times.map { ("A".."Z").to_a.sample }.join
+      numbers = 3.times.map { (1..9).to_a.sample.to_s }.join
+      letters + numbers
+    end
 end
 
