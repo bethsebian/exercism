52c7c90efffa74df4170cd009cace7b3c1ed0d7c
Beth Sebian <esebian@gmail.com>
2016-05-23 21:52:12 -0600

    Refactor ruby/nth-prime

--- ruby/nth-prime/nth_prime.rb
+++ ruby/nth-prime/nth_prime.rb
@@ -1,17 +1,17 @@
 class Prime
   def nth(place)
-    fail ArgumentError if place == 0
-    @primes = []
+    fail ArgumentError, "Zero is not a valid argument" if place == 0
+    @primes = [2]
     identify_primes(place)
     @primes.last
   end
 
   private
     def identify_primes(place)
-      next_num = 2
+      next_num = 3
       until @primes.length == place
         @primes << next_num unless @primes.find { |prime| next_num % prime == 0 }
-        next_num += 1
+        next_num += 2
       end
     end
 end
